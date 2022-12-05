4b437581f5f36a15287f9066f8e23b2fea1f15ec
Rafael <enterrafael@gmail.com>
2022-12-05 20:38:39 -0300

    First commit

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto de React.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção
+            de um cadastro desenvolvido em React!</p>
+            <br/>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, facere repudiandae quis aperiam nisi eos repellendus blanditiis 
+                facilis fugiat ipsa, atque ab? Vero aliquid amet repudiandae saepe veritatis, explicabo ad.</p>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, facere repudiandae quis aperiam nisi eos repellendus blanditiis 
+                facilis fugiat ipsa, atque ab? Vero aliquid amet repudiandae saepe veritatis, explicabo ad.</p>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, facere repudiandae quis aperiam nisi eos repellendus blanditiis 
+                facilis fugiat ipsa, atque ab? Vero aliquid amet repudiandae saepe veritatis, explicabo ad.</p>
+    </Main>
---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Raf<span className="text-danger">a</span>el <span className="text-danger">Florentino</span></strong>
+        </span>
+    </footer>
